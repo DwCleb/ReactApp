3617d02f83143a90dd2f1a8adacd2479503763c5
Cleber Junior <dwcleb@gmail.com>
2018-08-17 22:58:05 -0300

    Props

--- src/index.js
+++ src/index.js
@@ -1,5 +1,41 @@
-const func = (a, b) => {
-  return a + b;
+import React, { Component, Fragment } from 'react';
+import PropTypes from "prop-types";
+import { render } from 'react-dom';
+
+class Button extends Component {
+  render () {
+    return (
+      <a href="" onClick={this.props.onClick}>
+        {this.props.children}
+      </a>
+    );
+  }
+}
+
+Button.defaultProps = {
+  children: "Salve",
 };
 
-alert(func(3,6));
+Button.propTypes = {
+  onClick: PropTypes.func.isRequired,
+  children: PropTypes.string
+};
+
+class App extends Component {
+  handleClick() {
+    alert('Button Clicked');
+  }
+  render() {
+    return (
+      <Fragment>
+        <h1>ReactApp</h1>
+        <Button onClick={ () => {
+          alert("Button");  
+        }}/>
+        <Button onClick={this.handleClick}> Send </Button>
+      </Fragment>
+    );
+  }
+}
+
+render(<App />, document.getElementById('app'));
